5f60d40aae448f87dc5f65a5490416f83dc828a1
marcoreliodev <aurelius.sf@gmail.com>
2025-03-06 01:48:54 -0300

    feat: add error handling

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,25 @@
 import 'dotenv/config';
+import 'express-async-errors';
 
-import express from 'express';
+import AppError from './errors/AppError';
+
+import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log('Server is running on port 3333'));
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return void res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return void res.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message} `,
+  });
+});
+
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log(`Server is running on port ${port}.`));
