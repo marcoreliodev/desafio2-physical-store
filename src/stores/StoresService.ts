3b0b2a614879c4367ec74c4c5904c0e0b9a7f3d3
marcoreliodev <aurelius.sf@gmail.com>
2025-03-06 22:48:10 -0300

    feat(storesService): add listNearbyStoresByCep method

---
+++ src/stores/StoresService.ts
@@ -0,0 +1,62 @@
+import { ViaCepService } from '../services/ViaCepService';
+import { NominatimService } from '../services/NominatimService';
+import { OsrmService } from '../services/OsrmService';
+
+import AppError from '../errors/AppError';
+
+import { StoresRepository } from './StoresRepository';
+
+export class StoresService {
+  constructor(private storesRepository: StoresRepository) {}
+
+  async listNearbyStoresByCep(cep: string) {
+    const userLocation = await ViaCepService.getLocationByCep(cep);
+
+    if (!userLocation) {
+      throw new Error(
+        `Não foi possível encontrar a localização para o CEP: ${cep}`
+      );
+    }
+
+    const userCoordinates = await NominatimService.getCoordinates(userLocation);
+
+    if (!userCoordinates) {
+      throw new Error(
+        'Não foi possível recuperar as coordenadas para a localização informada.'
+      );
+    }
+
+    const radiusInKM = 100;
+
+    const nearbyStores = await this.storesRepository.getStoresWithinRadius(
+      userCoordinates,
+      radiusInKM
+    );
+
+    if (nearbyStores.length === 0) {
+      throw new AppError(
+        `Não há lojas próximas no raio de ${radiusInKM} Km`,
+        404
+      );
+    }
+
+    const distancePromises = nearbyStores.map(async (store) => {
+      const distance = await OsrmService.calculateRouteDistance(
+        userCoordinates,
+        {
+          lat: store.latitude,
+          lon: store.longitude,
+        }
+      );
+
+      return {
+        ...store,
+        distance: distance || `${store.distance.toFixed(2)} Km`,
+      };
+    });
+
+    const stores = await Promise.all(distancePromises);
+
+    return stores;
+  }
+}
